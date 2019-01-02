21f887463aba84ff1b6b31a2bc51a47f0351d0e9
jihwan8D <jihwan8D@gmail.com>
2019-01-02 18:27:25 +0900

    eclipse 설정 추가

---
+++ java-basic/src/main/java/ch01/Test03.java
@@ -0,0 +1,10 @@
+package ch01;
+public class Test03 {
+ /*   @Override // 애노테이션이라 부르는 주석이다. 프로그램에서 사용한다.
+    public Sting toString(){
+        return "ok";
+    } */
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
