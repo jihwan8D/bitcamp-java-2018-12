0f2c410bbfd98963cc5a14b55f9e9e9d6c82b7e2
jihwan8D <jihwan8D@gmail.com>
2019-01-28 23:53:48 +0900

    ArrayList 사용법

--- java-basic/src/main/java/ch20/a/Test05.java
+++ java-basic/src/main/java/ch20/a/Test05.java
@@ -5,18 +5,33 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Test05 {
-
+  
   public static void main(String[] args) {
     class Student {
-      /*   @Override
+      String name;
+      int age;
+      
+      public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+      }
+
+      @Override
+      public String toString() {
+        return "Student [name=" + name + ", age=" + age + "]";
+      }
+
+      /*
+      @Override
       public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + age;
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
-      }*/
-
+      }
+      */
+      
       @Override
       public boolean equals(Object obj) {
         if (this == obj)
@@ -35,44 +50,32 @@ public class Test05 {
           return false;
         return true;
       }
-
-      String name;
-      int age;
-
-      public Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-      }
-      @Override
-      public String toString() {
-        return "Student [name=" + name + ", age=" + age + "]";
-      }
-
-
     }
-
+    
     Student s1 = new Student("aaa", 20);
     Student s2 = new Student("bbb", 30);
     Student s3 = new Student("ccc", 40);
-
+    
     Student s4 = new Student("bbb", 30);
-
+    
     System.out.println(s2 == s4);
-    System.out.println(s2.equals(s4)); 
-    // 인스턴스는 다르지만 내용물은 같다.
+    System.out.println(s2.equals(s4));
     System.out.println(s2.hashCode() == s4.hashCode());
-
+    
     ArrayList<Student> list = new ArrayList<>();
     list.add(s1);
     list.add(s2);
     list.add(s3);
+    
+    
     System.out.println(list);
-
-    // indexOf(값)
+    
+    // indexOf(값) 
     // => 목록에 해당 값이 있는 인덱스를 알아낸다.
     // => 값을 비교할 때는 contains()와 마찬가지로 equals()의 리턴 값이 true인 경우 같은 값으로 간주한다.
-    System.out.println(list.indexOf(s2)); // 목록이 이런값을 갖고 있냐?
+    System.out.println(list.indexOf(s2));
     System.out.println(list.indexOf(s4));
     System.out.println(list.indexOf(new Student("ccc", 30))); // 못찾으면 -1 이다.
   }
+
 }
