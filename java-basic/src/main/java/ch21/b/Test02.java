507a814b4bf13b9b00f40844bef74ebff94db2ca
jihwan8D <jihwan8D@gmail.com>
2019-01-29 15:15:06 +0900

    예외처리

--- java-basic/src/main/java/ch21/b/Test02.java
+++ java-basic/src/main/java/ch21/b/Test02.java
@@ -1,31 +1,38 @@
-// 예외 처리하기 - catch 블록
+// 예외 처리하기 - 여러 개의 발생할 때, 멀티 catch 블록을 사용한다.
 package ch21.b;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Test02 {
   public static void main(String[] args) {
-
     Scanner keyboard = new Scanner(System.in);
-    System.out.print("파일명을 입력하세요: ");
-    String input = keyboard.nextLine();
 
-    try {
-      File file = new File(input); // NullPointException
+    HashMap<String, Command> commandMap = new HashMap<>();
+    commandMap.put("plus", new PlusCommand(keyboard));
 
-      FileInputStream in = new FileInputStream(file); // FileNotFoundException
+    while(true) {
+      System.out.print("명령> ");
+      String input = keyboard.nextLine();
+      if (input.equals("quit")) {
+        break;
+      }
 
-      int b = in.read(); // IOException
+      // 예외 처리 문법을 적용하면, 예외가 발생하더라도 JVM을 멈추지 않는다.
+      try {
+        Command command = commandMap.get(input);
+        command.execute();
 
-      in.close(); // IOException
-      System.out.println("실행 완료");
-    } catch(IOException e) {
-      // try 블록에서 발생한 예외 정보를 받으려면 catch 블록에 해당 값을 받을 파라미터를 선언해야 한다.
-      System.out.println(e.toString());
-    } 
+        // try 블록에서 발생할 수 있는 예외가 여러 개일 때
+        // 각 예외 정보를 받을 catch 블록을 여러 개 둘 수 있다.
+      } catch(NullPointerException e) {
+        System.out.println("해당 명령이 존재하지 않습니다.");
+      } catch(NumberFormatException e) {
+        System.out.println("숫자를 입력하세요.");
+      }
+
+    }
+    System.out.println("실행 완료");
     keyboard.close();
   }
 }
