a7d393cd013b9df82d6f8a9e49f7b0fc3de724f2
jihwan8D <jihwan8D@gmail.com>
2019-02-15 11:46:11 +0900

    .

---
+++ java-basic/src/main/java/design_pattern/proxy/before2/Server/Calculator.java
@@ -0,0 +1,14 @@
+package design_pattern.proxy.before2.Server;
+
+public class Calculator {
+  
+  public int plus(int a, int b) {
+    return a + b;
+  }
+  
+  public int minus(int a, int b) {
+    return a - b;
+  }
+  
+  
+}
