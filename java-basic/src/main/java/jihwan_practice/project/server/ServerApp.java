26a147f94533e6852b172de29e53dacd7fcd2187
jihwan8D <jihwan8D@gmail.com>
2019-02-17 14:20:23 +0900

    .

--- java-basic/src/main/java/jihwan_practice/project/server/ServerApp.java
+++ java-basic/src/main/java/jihwan_practice/project/server/ServerApp.java
@@ -4,34 +4,80 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import jihwan_practice.project.server.domain.Board;
 
 public class ServerApp {
 
+  static ObjectInputStream in;
+  static ObjectOutputStream out;
+  static ArrayList<Board> list = new ArrayList<>();
+
   public static void main(String[] args) {
+
 
     try (ServerSocket ss = new ServerSocket(8888)){
       System.out.println("서버 시작");
 
       while (true) {
-        try (Socket s = ss.accept();
-            ObjectInputStream in = new ObjectInputStream(s.getInputStream());
-            ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream())) {
-          System.out.println("클라이언트와 연결됨");
+        try (Socket socket = ss.accept();
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
+          
+          System.out.println("클라이언트와 연결되었음.");
+          list.clear();
+          ServerApp.in = in;
+          ServerApp.out = out;
+          
+          
+          loop: while (true) {
+              String request = in.readUTF();
+              System.out.println(request);
+              
+              switch (request) {
+                case "quit":
+                  quit();
+                  break loop;
+                case "add":
+                  add();
+                  break;
+                case "list":
+                  list();
+                  break;
+                default:
+                  out.writeUTF("이 명령을 처리할 수 없음!");
+              }
+              out.flush();
+          }
 
-          System.out.println(in.readUTF()); 
 
-          out.writeUTF("서버에서 보낸 메시지");
-          out.flush();
+
+
 
         } catch (Exception e) {
           e.printStackTrace();        
         }
         System.out.println("클라이언트와의 연결을 끊었음.");
-      }
+      } // while
 
     } catch (Exception e) {
       e.printStackTrace();
     }
+  } // main
+
+  static void quit() throws Exception {
+    out.writeUTF("종료함!");
+    out.flush();
   }
+  
+  static void add() throws Exception {
+    list.add((Board)in.readObject());
+    out.writeUTF("OK");
+  }
+  
+  static void list() throws Exception {
+    out.writeObject(list);
+  }
+
 }
 
--- java-basic/src/main/java/jihwan_practice/project/server/ServerTest.java
+++ java-basic/src/main/java/jihwan_practice/project/server/ServerTest.java
@@ -3,23 +3,66 @@ package jihwan_practice.project.server;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.List;
+import jihwan_practice.project.server.domain.Board;
 
 public class ServerTest {
 
+  static ObjectOutputStream out;
+  static ObjectInputStream in;
+
   public static void main(String[] args) {
-    
+
     try (Socket s = new Socket("localhost", 8888);
         ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(s.getInputStream())) {
-      
       System.out.println("서버와 연결됨");
-      
-      out.writeUTF("Hello");
+
+      ServerTest.in = in;
+      ServerTest.out = out;
+
+      out.writeUTF("okok");
       out.flush();
       System.out.println(in.readUTF());
+      add(new Board(1, "게시글 1"));
+      add(new Board(2, "게시글 2"));
+
+      list();
+
+      quit();
+
+
+
+
+
+
+
+
     } catch(Exception e) {
-      
+      e.printStackTrace();
     }
     System.out.println("서버와의 연결을 끊었음.");
+  } // main
+
+  public static void add(Board board) throws Exception {
+    out.writeUTF("add");
+    out.writeObject(board);
+    out.flush();
+    System.out.println(in.readUTF());
+  }
+
+  public static void list() throws Exception {
+    out.writeUTF("list");
+    out.flush();
+    List<Board> list = (List<Board>) in.readObject();
+    for(Board l : list) {
+      System.out.println(l);
+    }
+  }
+
+  public static void quit() throws Exception {
+    out.writeUTF("quit");
+    out.flush();
+    System.out.println(in.readUTF());
   }
 }
