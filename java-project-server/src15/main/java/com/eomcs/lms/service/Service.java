80532209d3fdd996c899ce03c1d7a96fcbd70743
jihwan8D <jihwan8D@gmail.com>
2019-02-18 12:59:27 +0900

    프로젝트 멀티스레드 적용

---
+++ java-project-server/src15/main/java/com/eomcs/lms/service/Service.java
@@ -0,0 +1,16 @@
+package com.eomcs.lms.service;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public interface Service {
+
+  void execute(String request, ObjectInputStream in, ObjectOutputStream out) throws Exception;
+}
+
+
+
+
+
+
+
