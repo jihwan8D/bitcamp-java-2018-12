ad4bb7776472b0667c17e1883b2fa63f542ed8be
jihwan8D <jihwan8D@gmail.com>
2019-03-04 16:08:00 +0900

    .

--- java-project2-server/src06/main/java/com/eomcs/lms/domain/Board.java
+++ java-project2-server/src06/main/java/com/eomcs/lms/domain/Board.java
@@ -1,28 +1,37 @@
 package com.eomcs.lms.domain;
-
 import java.io.Serializable;
 import java.sql.Date;
 
-public class PhotoBoard implements Serializable {
+public class Board implements Cloneable, Serializable {
   private static final long serialVersionUID = 1L;
-  
+
   private int no;
-  private String title;
+  private String contents;
   private Date createdDate;
   private int viewCount;
-  private int lessonNo;
   
+  @Override
+  public Board clone() throws CloneNotSupportedException {
+    return (Board) super.clone();
+  }
+  
+  @Override
+  public String toString() {
+    return "Board [no=" + no + ", contents=" + contents + ", createdDate=" + createdDate
+        + ", viewCount=" + viewCount + "]";
+  }
+
   public int getNo() {
     return no;
   }
   public void setNo(int no) {
     this.no = no;
   }
-  public String getTitle() {
-    return title;
+  public String getContents() {
+    return contents;
   }
-  public void setTitle(String title) {
-    this.title = title;
+  public void setContents(String contents) {
+    this.contents = contents;
   }
   public Date getCreatedDate() {
     return createdDate;
@@ -36,13 +45,6 @@ public class PhotoBoard implements Serializable {
   public void setViewCount(int viewCount) {
     this.viewCount = viewCount;
   }
-  public int getLessonNo() {
-    return lessonNo;
-  }
-  public void setLessonNo(int lessonNo) {
-    this.lessonNo = lessonNo;
-  }
-  
   
   
 }
