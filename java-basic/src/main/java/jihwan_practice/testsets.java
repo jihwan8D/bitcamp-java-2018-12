2425da51644cecff26dc177741aedb7c056be538
jihwan8D <jihwan8D@gmail.com>
2019-03-24 16:18:50 +0900

    문제풀이

---
+++ java-basic/src/main/java/jihwan_practice/testsets.java
@@ -0,0 +1,12 @@
+package jihwan_practice;
+
+public class testsets {
+
+  public static void main(String[] args) {
+
+    for(int i=0; i < 10; i++) {
+      System.out.println(i%5);
+    }
+  }
+
+}
