91e893e826fc5d787584515d1b37913785adc382
jihwan8D <jihwan8D@gmail.com>
2019-03-26 18:11:19 +0900

    tomcat적용

--- java-project2-server/src/main/java/com/eomcs/lms/handler/HelloCommand2.java
+++ java-project2-server/src/main/java/com/eomcs/lms/handler/HelloCommand2.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Component;
 import com.eomcs.lms.context.RequestMapping;
 
 @Component
-public class HelloCommand {
+public class HelloCommand2 {
   
-  @RequestMapping("/hello")
+  @RequestMapping("/hello2")
   public void list(ServletRequest request, ServletResponse response) throws Exception {
     PrintWriter out = response.getWriter();
     out.println("<html><head><title>hello</title></head>");
     out.println("<body><h1>Hello, world!</h1>");
     out.println("</body></html>");
-  }
+  }   
 }
 
 
